339182635784826e4b934c719adfdc1318d426a9
FANCY TUBA <158304347+Fancytuba06@users.noreply.github.com>
2024-03-13 16:19:46 +0530

    Updated securitygroup ec2-instance.tf

--- ec2-instance.tf
+++ ec2-instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "web" {
   instance_type = var.instance_type
   key_name      = var.keypair 
   vpc_security_group_ids = [
-    aws_security_group.sg.id,  
+    aws_security_group.sg.id  
      
     
   ]
@@ -13,5 +13,32 @@ resource "aws_instance" "web" {
 
   tags = {
     Name = var.ec2_Name
+  }
+}
+
+#security group 
+
+resource "aws_security_group" "sg" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = "${aws_vpc.dev.id}"
+
+  ingress {
+    description = "TLS from VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = var.ingr_cidr_blocks
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = var.engr_cidr_blocks
+  }
+
+  tags = {
+    Name = var.sg_Name
   }
 }
